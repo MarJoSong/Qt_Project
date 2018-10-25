46ed34c3bd5989f935490a5c41463e07f981c940
marjosong <769200142@qq.com>
2018-10-25 10:22:56 +0800

    添加打开文件槽函数

--- StandardItemModel/mainwindow.cpp
+++ StandardItemModel/mainwindow.cpp
@@ -1,6 +1,7 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include <QFileDialog>
+#include <QTextStream>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -51,5 +52,27 @@ void MainWindow::on_actOpen_triggered()
 {
     QString curPath = QCoreApplication::applicationDirPath();
     QString aFileName = QFileDialog::getOpenFileName(this, "打开一个文件",
+            curPath, "井数据文件(*.txt);;所有文件(*.*)");
+    if(aFileName.isEmpty())
+        return;
+
+    QStringList fFileContent;
+    QFile aFile(aFileName);
+    if(aFile.open(QIODevice::ReadOnly | QIODevice::Text))
+    {
+        QTextStream aStream(&aFile);
+        ui->plainTextEdit->clear();
+        while(!aStream.atEnd())
+        {
+            QString str = aStream.readLine();
+            ui->plainTextEdit->appendPlainText(str);
+            fFileContent.append(str);
+        }
+        aFile.close();
+        this->labCurFile->setText("当前文件: " + aFileName);
+        iniModelFromStringList(fFileContent);
+
+
+    }
 
 }
